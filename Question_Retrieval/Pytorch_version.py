b9a9ee440d4054e364ab83a53f97465bd76dd65d
mxy2008 <xinyima9@gmail.com>
2017-12-02 00:39:41 -0500

    wenxuan version

--- Question_Retrieval/Pytorch_version.py
+++ Question_Retrieval/Pytorch_version.py
@@ -20,7 +20,7 @@ import utils
 
 
 # # Model
-args = {'dropout':0.1, 'hidden_dim':200, 'depth':1, 'average':False, 'dev': 'askubuntu/dev.txt', \
+args = {'dropout':0.2, 'hidden_dim':240, 'depth':1, 'average':True, 'dev': 'askubuntu/dev.txt', \
         'test': 'askubuntu/test.txt', 'normalize':0, 'l2_reg': 1e-5}
 
 
@@ -48,6 +48,8 @@ say("vocab size={}, corpus size={}\n".format(embedding_layer.n_V,len(raw_corpus)
 padding_id = embedding_layer.vocab_map["<padding>"]
 print 'padding_id', padding_id
 
+# if args['reweight']:
+    # weights = create_idf_weights('askubuntu/text_tokenized.txt.gz', embedding_layer)
 
 # # Create training batches
 train = read_annotations('askubuntu/train_random.txt')
@@ -66,6 +68,8 @@ say("{} batches, {} tokens in total, {} triples in total\n".format(
 train_batches = None
 
 
+
+
 class LSTM(nn.Module):
     def __init__(self, args, embedding_layer, weights=None):
         super(LSTM, self).__init__()
@@ -78,6 +82,8 @@ class LSTM(nn.Module):
         n_e = self.n_e = embedding_layer.n_d # embedding dimension
         depth = self.depth = args['depth'] # layer depth
         dropout = self.dropout = args['dropout']
+        self.tanh = nn.Tanh()
+
 
         self.lstm = nn.LSTM(
             input_size = n_e,
@@ -85,7 +91,10 @@ class LSTM(nn.Module):
             num_layers = depth,
             dropout = dropout,
         )
-        
+
+        self.hidden_1 = None
+        self.hidden_2 = None
+
     def init_hidden(self, batch_size):
         # Before we've done anything, we dont have any hidden state.
         # Refer to the Pytorch documentation to see exactly
@@ -93,6 +102,7 @@ class LSTM(nn.Module):
         # The axes semantics are (num_layers, minibatch_size, hidden_dim)
         return (autograd.Variable(torch.zeros(1, batch_size, self.n_d)),
                 autograd.Variable(torch.zeros(1, batch_size, self.n_d)))
+
 
     def forward(self, idts, idbs):
         # embedding layer
@@ -114,10 +124,9 @@ class LSTM(nn.Module):
         # lstm
         # output_t, (ht, ct) = self.lstm(xt, None)
         # output_b, (hb, cb) = self.lstm(xb, None)
-        output_t, self.hidden_1 = self.lstm(xt, self.hidden_1)
-        output_b, self.hidden_2 = self.lstm(xb, self.hidden_2)
+        output_t, self.hidden_1 = self.lstm(xt, (self.tanh(self.hidden_1[0]), self.tanh(self.hidden_1[1])))
+        output_b, self.hidden_2 = self.lstm(xb, (self.tanh(self.hidden_2[0]), self.tanh(self.hidden_2[1])))
 
-        
         # self.ht = output_t#ht
         # self.hb = output_b#hb
         # TODO: add pooling with no padding.
@@ -141,7 +150,9 @@ class LSTM(nn.Module):
 
 # initialize the model
 lstm = LSTM(args, embedding_layer)
+
 optimizer = optim.Adagrad(lstm.parameters(), lr=0.001)
+
 # get the number of parameters of the model
 num_params = 0
 for layer in lstm.parameters():
@@ -170,14 +181,53 @@ def customized_loss(h_final, idps):
     l2_reg = None
     for layer in lstm.parameters():
         if l2_reg is None:
-            l2_reg = LA.norm(layer.data.numpy(), 2)
+            l2_reg = torch.norm(layer.data, 2)
         else:
-            l2_reg = l2_reg + LA.norm(layer.data.numpy(), 2)
+            l2_reg = l2_reg + torch.norm(layer.data, 2)
 
     l2_reg = l2_reg * args['l2_reg']
 #   self.cost = self.loss + l2_reg
-    loss  += l2_reg
-    return loss
+    cost  = loss + l2_reg
+    return loss, cost
+
+
+def customized_loss(h_final, idps):
+
+    h_final = torch.squeeze(h_final)
+    
+    xp = h_final[torch.from_numpy(idps.ravel()).long()]
+    xp = xp.view((idps.shape[0], idps.shape[1], args['hidden_dim']))
+    # num query * n_d
+    query_vecs = xp[:,0,:]
+     # num query
+    
+    q_len = torch.sqrt(torch.sum(query_vecs*query_vecs, dim=1))
+    p_len = torch.sqrt(torch.sum(xp[:,1,:]*xp[:,1,:], dim=1))
+    pos_scores = torch.sum(query_vecs*xp[:,1,:], dim=1)/(q_len*p_len)
+
+    x_len = torch.sqrt(torch.sum(torch.unsqueeze(query_vecs, dim=1)*torch.unsqueeze(query_vecs, dim=1), dim=2))
+    y_len = torch.sqrt(torch.sum(xp[:,2:,:]*xp[:,2:,:], dim=2))
+    # num query * candidate size
+    neg_scores = torch.sum(torch.unsqueeze(query_vecs, dim=1)*xp[:,2:,:], dim=2)/(x_len*y_len)
+    # num query
+    neg_scores = torch.max(neg_scores, dim=1)[0]
+    diff = neg_scores - pos_scores + 1.0
+    loss = torch.mean((diff>0).float()*diff)
+    
+    # TODO: add regularization
+    l2_reg = None
+    for layer in lstm.parameters():
+        if l2_reg is None:
+            l2_reg = torch.norm(layer.data, 2)
+        else:
+            l2_reg = l2_reg + torch.norm(layer.data, 2)
+
+    l2_reg = l2_reg * args['l2_reg']
+#   self.cost = self.loss + l2_reg
+    cost  = loss + l2_reg
+    return loss, cost
+
+
 
 def evaluate(data):
     lstm.eval()
@@ -208,35 +258,37 @@ test = read_annotations(args['test'], K_neg=-1, prune_pos_cnt=-1)
 
 for i in range(20):
     start_time = time.time()
-    #train = read_annotations('askubuntu/train_random.txt')
-    #dev = read_annotations(args['dev'], K_neg=-1, prune_pos_cnt=-1)
-    #test = read_annotations(args['test'], K_neg=-1, prune_pos_cnt=-1)
+    train = read_annotations('askubuntu/train_random.txt')
+    dev = read_annotations(args['dev'], K_neg=-1, prune_pos_cnt=-1)
+    test = read_annotations(args['test'], K_neg=-1, prune_pos_cnt=-1)
     train_batches = create_batches(ids_corpus, train, batch_size,
                                 padding_id, pad_left = not args['average'])
     
-    dev_batches = create_eval_batches(ids_corpus, dev, padding_id, pad_left = not args['average'])
-    test_batches = create_eval_batches(ids_corpus, test, padding_id, pad_left = not args['average'])
+    dev = create_eval_batches(ids_corpus, dev, padding_id, pad_left = not args['average'])
+    test = create_eval_batches(ids_corpus, test, padding_id, pad_left = not args['average'])
 
     N =len(train_batches)
     avrg_loss = 0
+    avrg_cost = 0
     for j in xrange(N):
-        #print i, j
         # get current batch
-        idts, idbs, idps = train_batches[i]
+        idts, idbs, idps = train_batches[j]
         optimizer.zero_grad()
         lstm.hidden_1 = lstm.init_hidden(idts.shape[1])
         lstm.hidden_2 = lstm.init_hidden(idbs.shape[1])
         
         h_final = lstm(idts, idbs)      # lstm output
-        loss = customized_loss(h_final, idps)     
-        loss.backward()                 # backpropagation, compute gradients
+        loss, cost = customized_loss(h_final, idps)     
+        cost.backward()                 # backpropagation, compute gradients
         optimizer.step()                # apply gradients
         avrg_loss += loss.data.numpy()[0]
+        avrg_cost += cost.data.numpy()[0]
+        print i, j, loss.data.numpy()[0], cost.data.numpy()[0]
         if j == N-1:
-            dev_MAP, dev_MRR, dev_P1, dev_P5 = evaluate(dev_batches)
-            test_MAP, test_MRR, test_P1, test_P5 = evaluate(test_batches)
+            dev_MAP, dev_MRR, dev_P1, dev_P5 = evaluate(dev)
+            test_MAP, test_MRR, test_P1, test_P5 = evaluate(test)
             print "running time:", time.time() - start_time
             print "epoch", i
-            print "train loss", avrg_loss*1.0/N
+            print "train loss", avrg_loss*1.0/N, avrg_cost*1.0/N
             print "dev", dev_MAP, dev_MRR, dev_P1, dev_P5
             print "test", test_MAP, test_MRR, test_P1, test_P5
